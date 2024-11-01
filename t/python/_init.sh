b2f46ed9f0f7f0540ede272e61654ec50e77fffd
4MBL <74970679+4mbl@users.noreply.github.com>
2024-11-01 10:48:32 +0200

    sparse checkout python subdir, fix virtual environment activation, style reminder prints

--- t/python/_init.sh
+++ t/python/_init.sh
@@ -1,6 +1,27 @@
+# read project name from command line argument
+
+project_name=$1
+
+prev_cwd=$PWD
+
+if [ -z "$project_name" ]; then
+    echo "Usage: $0 <project-name>"
+    exit 1
+fi
+
+# clone base
+git clone --no-checkout https://github.com/4mbl/templates $project_name
+cd $project_name
+git sparse-checkout init --cone
+git sparse-checkout set t/python/
+git checkout main
+rm -rf .git
+mv t/python/* .
+rm -rf t
+
 # virtualenv
-virtualenv .venv || python3 -m venv .venv
-.venv/scripts/activate
+(virtualenv .venv || python3 -m venv .venv) && . .venv/bin/activate
+pythom3 -m pip install -e .
 
 # readme
 curl https://4mbl.link/files/readme-template -Ls --output README.md
@@ -13,6 +34,11 @@ mkdir -p tests/
 
 
 # reminders
-echo "Search for 'TODO' in the project and update the configuration according to your project."
-echo "New project defaults to MIT license, make sure this is okay for the project."
-echo "Python modules should use short, lowercase names for modules, preferably without underscores (PEP8)."
+echo ""
+echo "# Reminders"
+echo "* Search for 'TODO' in the project and update the configuration according to your project."
+echo "* New project defaults to MIT license, make sure this is okay for the project."
+echo "* Python modules should use short, lowercase names for modules, preferably without underscores (PEP8)."
+echo ""
+
+cd $prev_cwd
