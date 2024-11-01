7280dd17586fa0a1378078601a4f94d743ed76bb
4MBL <74970679+4mbl@users.noreply.github.com>
2024-11-01 10:18:22 +0200

    use readme and changelog templates for python projects

--- t/python/_init.sh
+++ t/python/_init.sh
@@ -2,5 +2,11 @@
 virtualenv .venv || python3 -m venv .venv
 .venv/scripts/activate
 
+# readme
+curl https://4mbl.link/files/readme-template -Ls --output README.md
+
+# changelog
+curl https://4mbl.link/files/changelog-template -Ls --output CHANGELOG.md
+
 # testing
 mkdir -p tests/
