acd2d75d20872a4ae7007e997ed7aa5baa0aa0d2
4MBL <74970679+4mbl@users.noreply.github.com>
2024-11-01 10:33:20 +0200

    add reminder prints

--- t/python/_init.sh
+++ t/python/_init.sh
@@ -10,3 +10,9 @@ curl https://4mbl.link/files/changelog-template -Ls --output CHANGELOG.md
 
 # testing
 mkdir -p tests/
+
+
+# reminders
+echo "Search for 'TODO' in the project and update the configuration according to your project."
+echo "New project defaults to MIT license, make sure this is okay for the project."
+echo "Python modules should use short, lowercase names for modules, preferably without underscores (PEP8)."
